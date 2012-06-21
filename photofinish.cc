ca23febbae13b5a5d5a832a03c3801bcace596d3
Ian Tester <imroykun@gmail.com>
2012-06-22 00:10:10 +1000

    Despite what the documents say, reading the source revealed that compression_type really must be passed to png_get_iCCP. Add write_png method for writing PNG images. Need to do write_jpeg next. Swap the endian of 16-bit samples. How to do this only on little-endian machines? Comment out some debug messages. Still many remain.

--- photofinish.cc
+++ photofinish.cc
@@ -17,13 +17,21 @@
 	along with Photo Finish.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "Image.hh"
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  Image image1("/tmp/test.png");
-  //  Image<unsigned char> image1(1024, 768, NULL, TYPE_RGBA_16);
-  //  Image inter1(image1);
+  Image image1("test.png");
 
-  //  inter1.transform_from<unsigned short int>(image1, INTENT_PERCEPTUAL, 0);
+  unsigned int w, h;
+  w = image1.width();
+  h = image1.height();
+  for (unsigned int y = 0; y < h; y++)
+    for (unsigned int x = 0; x < w; x++) {
+      double *p = image1.at(x, y);
+      fprintf(stderr, "at(%d, %d) = (%f, %f, %f)\n", x, y, p[0], p[1], p[2]);
+    }
+
+  image1.write_png("test.out.png", 8, NULL, INTENT_PERCEPTUAL);
 
   return 0;
 }
