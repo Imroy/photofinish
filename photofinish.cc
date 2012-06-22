034d071d917a50343e8328ee074e4d3c65f88bf4
Ian Tester <imroykun@gmail.com>
2012-06-22 14:35:54 +1000

    Add method to resize the image. Doesn't work properly. Add OpenMP pragmas/include/lib. The LCMS transforms and rescaling are now done in parallel across all available CPUs.

--- photofinish.cc
+++ photofinish.cc
@@ -20,8 +20,9 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  Image image1("test.png");
+  Image image1("test3.png");
 
+  /*
   unsigned int w, h;
   w = image1.width();
   h = image1.height();
@@ -30,8 +31,12 @@ int main(int argc, char* argv[]) {
       double *p = image1.at(x, y);
       fprintf(stderr, "at(%d, %d) = (%f, %f, %f)\n", x, y, p[0], p[1], p[2]);
     }
+  */
 
-  image1.write_png("test.out.png", 8, NULL, INTENT_PERCEPTUAL);
+  //  image1.write_png("test3.out.png", 8, NULL, INTENT_PERCEPTUAL);
+
+  Image image2 = image1.resize(800, -1, 3);
+  image2.write_png("test3.scaled.png", 8, NULL, INTENT_PERCEPTUAL);
 
   return 0;
 }
