23e8d7c84af0a105c8f62e7d27972a6577e9d76d
Ian Tester <imroykun@gmail.com>
2012-06-22 17:49:00 +1000

    Display the number of threads OpenMP is using before transforming and rescaling.

---
+++ include/Resampler.hh
@@ -0,0 +1,73 @@
+/*
+	Copyright 2012 Ian Tester
+
+	This file is part of Photo Finish.
+
+	Photo Finish is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Photo Finish is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Photo Finish.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __RESAMPLER_HH__
+#define __RESAMPLER_HH__
+
+#include <stdlib.h>
+#include <math.h>
+
+// Currently only using Lanczos sampling
+class Resampler {
+private:
+  double _a, _ra;
+  double _from_size, _to_size;
+  unsigned int *_N, **_Position;
+  double **_Weight;
+  unsigned int _to_size_i, _factors;
+
+public:
+  Resampler(double a, double from, double to);
+  ~Resampler();
+
+  // Methods for accessing the private data
+  inline double from_size(void) {
+    return _from_size;
+  }
+
+  inline double to_size(void) {
+    return _to_size;
+  }
+
+  inline unsigned int factors(void) {
+    return _factors;
+  }
+
+  inline unsigned int& N(unsigned int i) {
+    return _N[i];
+  }
+
+  inline unsigned int* Position(unsigned int i) {
+    return _Position[i];
+  }
+
+  inline unsigned int& Position(unsigned int f, unsigned int i) {
+    return _Position[i][f];
+  }
+
+  inline double* Weight(unsigned int i) {
+    return _Weight[i];
+  }
+
+  inline double& Weight(unsigned int f, unsigned int i) {
+    return _Weight[i][f];
+  }
+
+};
+
+#endif // __RESAMPLER_HH__
