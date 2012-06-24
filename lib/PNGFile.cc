db1dda833b3b90c9c05790611000959baacf1fda
Ian Tester <imroykun@gmail.com>
2012-06-25 00:40:33 +1000

    Read PNG file progressively while transforming the image data in parallel threads.

--- lib/PNGFile.cc
+++ lib/PNGFile.cc
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <omp.h>
 #include <lcms2.h>
+#include <queue>
+#include <unistd.h>
 #include "ImageFile.hh"
 #include "Image.hh"
 
@@ -43,74 +45,48 @@ bool PNGFile::set_profile(cmsHPROFILE profile, cmsUInt32Number intent) {
   return true;
 }
 
-Image* PNGFile::read(void) {
-  fprintf(stderr, "Opening file \"%s\"...\n", _filepath);
-  FILE *fp = fopen(_filepath, "r");
-  if (!fp) {
-    fprintf(stderr, "Could not open file \"%s\": %s\n", _filepath, strerror(errno));
-    exit(1);
-  }
+struct pngrow_t {
+  png_uint_32 row_num;
+  png_bytep row_data;
 
-  {
-    unsigned char header[8];
-    //    fprintf(stderr, "Reading header...\n");
-    fread(header, 1, 8, fp);
-    if (png_sig_cmp(header, 0, 8)) {
-      fprintf(stderr, "File \"%s\" is not a PNG file.\n", _filepath);
-      exit(2);
-    }
-  }
+  inline pngrow_t(png_uint_32 rn, png_bytep rd) : row_num(rn), row_data(rd) {}
+};
 
-  //  fprintf(stderr, "Creating read structure...\n");
-  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-					   NULL, NULL, NULL);
-  if (!png) {
-    fprintf(stderr, "Could not initialize PNG read structure.\n");
-    exit(3);
-  }
+struct callback_state {
+  bool finished;
+  std::queue<pngrow_t*> rowqueue;
+  size_t rowlen;
+  omp_lock_t *queue_lock;
+  unsigned int width, height;
+  Image *img;
+  cmsHTRANSFORM transform;
+};
 
-  //  fprintf(stderr, "Creating info structure...\n");
-  png_infop info = png_create_info_struct(png);
-  if (!info) {
-    png_destroy_read_struct(&png, (png_infopp)NULL, (png_infopp)NULL);
-    exit(3);
-  }
-
-  //  fprintf(stderr, "Setting jump point...\n");
-  if (setjmp(png_jmpbuf(png))) {
-    png_destroy_read_struct(&png, &info, NULL);
-    fclose(fp);
-    fprintf(stderr, "something went wrong reading the PNG.\n");
-    exit(4);
-  }
-
-  //  fprintf(stderr, "Initialising PNG IO...\n");
-  png_init_io(png, fp);
-
-  png_set_sig_bytes(png, 8);
+void info_callback(png_structp png, png_infop info) {
   png_set_gamma(png, 1.0, 1.0);
   png_set_alpha_mode(png, PNG_ALPHA_PNG, 1.0);
+  png_set_packing(png);
+  png_set_swap(png);
+  png_read_update_info(png, info);
 
-  fprintf(stderr, "Reading PNG image...\n");
-  png_read_png(png, info, PNG_TRANSFORM_PACKING | PNG_TRANSFORM_SWAP_ENDIAN, NULL);
-  //  fprintf(stderr, "Getting rows...\n");
-  png_bytepp png_rows = png_get_rows(png, info);
+  callback_state *cs = (callback_state*)png_get_progressive_ptr(png);
 
-  //  fprintf(stderr, "Getting the header information...\n");
-  unsigned int width, height;
   int bit_depth, colour_type;
-  png_get_IHDR(png, info, &width, &height, &bit_depth, &colour_type, NULL, NULL, NULL);
-  fprintf(stderr, "%dx%d, %d bpp, type %d.\n", width, height, bit_depth, colour_type);
+  fprintf(stderr, "info_callback: Getting header information...\n");
+  png_get_IHDR(png, info, &cs->width, &cs->height, &bit_depth, &colour_type, NULL, NULL, NULL);
+  fprintf(stderr, "%dx%d, %d bpp, type %d.\n", cs->width, cs->height, bit_depth, colour_type);
 
-  Image *img = new Image(width, height);
+  cs->img = new Image(cs->width, cs->height);
 
   cmsUInt32Number cmsType;
   switch (colour_type) {
   case PNG_COLOR_TYPE_GRAY:
     cmsType = COLORSPACE_SH(PT_GRAY) | CHANNELS_SH(1) | BYTES_SH(bit_depth >> 3);
+    cs->rowlen = cs->width * (bit_depth >> 3);
     break;
   case PNG_COLOR_TYPE_RGB:
     cmsType = COLORSPACE_SH(PT_RGB) | CHANNELS_SH(3) | BYTES_SH(bit_depth >> 3);
+    cs->rowlen = cs->width * 3 * (bit_depth >> 3);
     break;
   default:
     fprintf(stderr, "unsupported PNG colour type %d\n", colour_type);
@@ -136,31 +112,131 @@ Image* PNGFile::read(void) {
     profile = cmsCreate_sRGBProfile();
   }
 
-  //  fprintf(stderr, "Creating colour transform...\n");
-  cmsHTRANSFORM transform = cmsCreateTransform(profile, cmsType,
-					       lab, IMAGE_TYPE,
-					       INTENT_PERCEPTUAL, 0);
+  fprintf(stderr, "Creating colour transform...\n");
+  cs->transform = cmsCreateTransform(profile, cmsType,
+				     lab, IMAGE_TYPE,
+				     INTENT_PERCEPTUAL, 0);
   cmsCloseProfile(lab);
   cmsCloseProfile(profile);
+}
+
+void row_callback(png_structp png, png_bytep row_data, png_uint_32 row_num, int pass) {
+  callback_state *cs = (callback_state*)png_get_progressive_ptr(png);
+  png_bytep new_row = (png_bytep)malloc(cs->rowlen);
+  memcpy(new_row, row_data, cs->rowlen);
+  pngrow_t *row = new pngrow_t(row_num, new_row);
+
+  //  int th_id = omp_get_thread_num();
+  omp_set_lock(cs->queue_lock);
+  cs->rowqueue.push(row);
+  omp_unset_lock(cs->queue_lock);
+}
+
+void end_callback(png_structp png, png_infop info) {
+  //  callback_state *cs = (callback_state*)png_get_progressive_ptr(png);
+}
+
+void worker(callback_state* cs) {
+  //  int th_id = omp_get_thread_num();
+  while (!(cs->rowqueue.empty() && cs->finished)) {
+    while (cs->rowqueue.empty() && !cs->finished)
+      usleep(100);
+    if (!cs->rowqueue.empty()) {
+      omp_set_lock(cs->queue_lock);
+      pngrow_t *row = cs->rowqueue.front();
+      cs->rowqueue.pop();
+      omp_unset_lock(cs->queue_lock);
+
+      cmsDoTransform(cs->transform, row->row_data, cs->img->row(row->row_num), cs->width);
+      free(row->row_data);
+      delete row;
+    }
+  }
+}
+
+Image* PNGFile::read(void) {
+  fprintf(stderr, "Opening file \"%s\"...\n", _filepath);
+  FILE *fp = fopen(_filepath, "r");
+  if (!fp) {
+    fprintf(stderr, "Could not open file \"%s\": %s\n", _filepath, strerror(errno));
+    exit(1);
+  }
+
+  {
+    unsigned char header[8];
+    //    fprintf(stderr, "Reading header...\n");
+    fread(header, 1, 8, fp);
+    if (png_sig_cmp(header, 0, 8)) {
+      fprintf(stderr, "File \"%s\" is not a PNG file.\n", _filepath);
+      exit(2);
+    }
+    fseek(fp, 0, SEEK_SET);
+  }
+
+  fprintf(stderr, "Creating read structure...\n");
+  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+					   NULL, NULL, NULL);
+  if (!png) {
+    fprintf(stderr, "Could not initialize PNG read structure.\n");
+    exit(3);
+  }
+
+  fprintf(stderr, "Creating info structure...\n");
+  png_infop info = png_create_info_struct(png);
+  if (!info) {
+    png_destroy_read_struct(&png, (png_infopp)NULL, (png_infopp)NULL);
+    exit(3);
+  }
+
+  fprintf(stderr, "Setting jump point...\n");
+  if (setjmp(png_jmpbuf(png))) {
+    png_destroy_read_struct(&png, &info, NULL);
+    fclose(fp);
+    fprintf(stderr, "something went wrong reading the PNG.\n");
+    exit(4);
+  }
 
 #pragma omp parallel
   {
 #pragma omp master
     {
-      fprintf(stderr, "Transforming image data to L*a*b* using %d threads...\n", omp_get_num_threads());
+      // We need at least two threads, so force it on uniprocessor systems
+      if (omp_get_num_threads() < 2)
+	omp_set_num_threads(2);
     }
   }
-#pragma omp parallel for schedule(dynamic, 1)
-  for (unsigned int y = 0; y < height; y++)
-    cmsDoTransform(transform, png_rows[y], img->row(y), width);
 
-  cmsDeleteTransform(transform);
+  callback_state cs;
+  cs.queue_lock = (omp_lock_t*)malloc(sizeof(omp_lock_t));
+  omp_init_lock(cs.queue_lock);
+  cs.width = cs.height = 0;
+  cs.transform = NULL;
+  cs.finished = false;
+#pragma omp parallel shared(cs)
+  {
+    int th_id = omp_get_thread_num();
+    if (th_id == 0) {	// Master thread
+      fprintf(stderr, "%d: Reading PNG image and transforming into L*a*b* using %d threads...\n", th_id, omp_get_num_threads());
+      png_set_progressive_read_fn(png, (void *)&cs, info_callback, row_callback, end_callback);
+      png_byte buffer[1048576];
+      size_t length;
+      while ((length = fread(buffer, 1, 1048576, fp)))
+	png_process_data(png, info, buffer, length);
+      cs.finished = true;
+      worker(&cs);	// Help finish off the transforming of image data
+    } else {	// Worker threads
+      worker(&cs);
+    }
+  }
+  omp_destroy_lock(cs.queue_lock);
+  free(cs.queue_lock);
+  cmsDeleteTransform(cs.transform);
 
   fprintf(stderr, "Done.\n");
   png_destroy_read_struct(&png, &info, NULL);
   fclose(fp);
 
-  return img;
+  return cs.img;
 }
 
 bool PNGFile::write(Image* img) {
