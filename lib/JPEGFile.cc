ee1bd1fcdb4ca4edbafe30ff7570ac00e3c64ff1
Ian Tester <imroykun@gmail.com>
2012-06-25 16:40:01 +1000

    Fix string operations looking at the ending of a filepath to determine the type.

--- lib/JPEGFile.cc
+++ lib/JPEGFile.cc
@@ -16,8 +16,13 @@
 	You should have received a copy of the GNU General Public License
 	along with Photo Finish.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <stdio.h>
+#include <jpeglib.h>
+#include <setjmp.h>
 #include <omp.h>
+#include <lcms2.h>
 #include "ImageFile.hh"
+#include "Image.hh"
 
 JPEGFile::JPEGFile(const char* filepath) :
   _ImageFile(filepath)
@@ -38,6 +43,48 @@ Image* JPEGFile::read(void) {
 }
 
 bool JPEGFile::write(Image* img) {
-  return false;
+  jpeg_compress_struct cinfo;
+  jpeg_error_mgr jerr;
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+
+  fprintf(stderr, "Opening file \"%s\"...\n", _filepath);
+  FILE *fp = fopen(_filepath, "wb");
+  if (!fp) {
+    fprintf(stderr, "can't open file \"%s\"\n", _filepath);
+    return false;
+  }
+  jpeg_stdio_dest(&cinfo, fp);
+
+  cinfo.image_width = img->width();
+  cinfo.image_height = img->height();
+  cinfo.input_components = 3;
+  cinfo.in_color_space = JCS_RGB;
+  jpeg_set_defaults(&cinfo);
+  jpeg_set_quality(&cinfo, 95, TRUE);
+
+  cmsHPROFILE lab = cmsCreateLab4Profile(NULL);
+  cmsHPROFILE sRGB = cmsCreate_sRGBProfile();
+  cmsHTRANSFORM transform = cmsCreateTransform(lab, IMAGE_TYPE,
+					       sRGB, TYPE_RGB_8,
+					       INTENT_PERCEPTUAL, 0);
+  cmsCloseProfile(lab);
+  cmsCloseProfile(sRGB);
+
+  JSAMPROW row[1];
+  row[0] = (JSAMPROW)malloc(img->width() * 3 * sizeof(JSAMPLE));
+
+  fprintf(stderr, "Writing JPEG file...\n");
+  jpeg_start_compress(&cinfo, TRUE);
+  while (cinfo.next_scanline < cinfo.image_height) {
+    cmsDoTransform(transform, img->row(cinfo.next_scanline), row[0], img->width());
+    jpeg_write_scanlines(&cinfo, row, 1);
+  }
+
+  jpeg_finish_compress(&cinfo);
+  fclose(fp);
+  jpeg_destroy_compress(&cinfo);
+
+  return true;
 }
 
