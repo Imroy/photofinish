5fb23ff1f2acbc1704942f5c7e919b76167da805
Ian Tester <imroykun@gmail.com>
2012-07-01 05:14:20 +1000

    Don't copy image data in the copy constructor, simply copy the rowdata pointer. Use shared_ptr to do reference counting of the new private "Rows" structure. Add a copy_pixels() method to actually copy the pixel data. Not used.

--- lib/Image.cc
+++ lib/Image.cc
@@ -3,38 +3,76 @@
 
 namespace PhotoFinish {
 
+  Image::Rows::Rows() :
+    rows(0),
+    rowdata(NULL)
+  {}
+
+  Image::Rows::Rows(long int rs, long int rowsize) :
+    rows(rs)
+  {
+    rowdata = (SAMPLE**)malloc(rows * sizeof(SAMPLE*));
+    for (long int r = 0; r < rows; r++)
+      rowdata[r] = (SAMPLE*)malloc(rowsize * sizeof(SAMPLE));
+  }
+
+  Image::Rows::Rows(const Rows& other) :
+    rowdata(other.rowdata)
+  {}
+
+  Image::Rows::~Rows() {
+    if (rowdata != NULL) {
+      for (long int r = 0; r < rows; r++)
+	free(rowdata[r]);
+      free(rowdata);
+      rowdata = NULL;
+    }
+  }
+
+
+
+  Image::Image() :
+    _width(-1),
+    _height(-1),
+    _greyscale(false),
+    _rows(NULL)
+  {}
+
   Image::Image(long int w, long int h) :
     _width(w),
     _height(h),
     _greyscale(false),
-    rowdata(NULL)
-  {
-    rowdata = (SAMPLE**)malloc(_height * sizeof(SAMPLE*));
-    for (long int y = 0; y < _height; y++)
-      rowdata[y] = (SAMPLE*)malloc(_width * 3 * sizeof(SAMPLE));
-  }
+    _rows(new Rows(h, w * 3))
+  {}
 
   Image::Image(const Image& other) :
     _width(other._width),
     _height(other._height),
     _greyscale(other._greyscale),
-    rowdata(NULL)
-  {
-    rowdata = (SAMPLE**)malloc(_height * sizeof(SAMPLE*));
-    for (long int y = 0; y < _height; y++) {
-      rowdata[y] = (SAMPLE*)malloc(_width * 3 * sizeof(SAMPLE));
-      memcpy(rowdata[y], other.rowdata[y], _width * 3 * sizeof(SAMPLE));
-    }
-  }
+    _rows(other._rows)
+  {}
 
   Image::~Image() {
-    if (rowdata != NULL) {
-      for (long int y = 0; y < _height; y++)
-	free(rowdata[y]);
-      free(rowdata);
-      rowdata = NULL;
+  }
+
+  Image& Image::operator=(const Image& b) {
+    if (this != &b) {
+      _width = b._width;
+      _height = b._height;
+      _greyscale = b._greyscale;
+      _rows = b._rows;
     }
-    _width = _height = 0;
+
+    return *this;
+  }
+
+  void Image::copy_pixels(void) {
+    std::shared_ptr<Rows> new_rows(new Rows(_height, _width * 3));
+
+    for (long int y = 0; y < _height; y++)
+      memcpy(new_rows->rowdata[y], _rows->rowdata[y], _width * 3 * sizeof(SAMPLE));
+
+    _rows.swap(new_rows);
   }
 
 }
