4537a1802477672768fbeb61a05485c0e52381bc
Ian Tester <imroykun@gmail.com>
2012-07-01 05:14:20 +1000

    Don't copy image data in the copy constructor, simply copy the rowdata pointer. Use shared_ptr to do reference counting of the new private "Rows" structure. Add a copy_pixels() method to actually copy the pixel data. Not used.

--- include/Image.hh
+++ include/Image.hh
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <memory>
 
 namespace PhotoFinish {
 
@@ -30,12 +31,28 @@ namespace PhotoFinish {
   private:
     long int _width, _height;
     bool _greyscale;		// Used by readers and writers when converting colour spaces
-    SAMPLE **rowdata;
+
+    struct Rows {
+      long int rows;
+      SAMPLE ** rowdata;
+
+      Rows();
+      Rows(long int rows, long int rowsize);
+      Rows(const Rows& other);
+      ~Rows();
+    };
+
+    std::shared_ptr<Rows> _rows;
 
   public:
+    Image();
     Image(long int w, long int h);
-    Image(const Image& other);
+    Image(const Image& other);		// Note: links to other image's data
     ~Image();
+
+    Image& operator=(const Image& b);
+
+    void copy_pixels(void);		// Actually copy the image data
 
     // Methods for accessing the private data
     inline long int width(void) const { return _width; }
@@ -44,12 +61,11 @@ namespace PhotoFinish {
     inline bool is_colour(void) const { return !_greyscale; }
     inline void set_greyscale(bool g = true) { _greyscale = g; }
     inline void set_colour(bool c = true) { _greyscale = !c; }
-    inline SAMPLE* row(long int y) const { return rowdata[y]; }
-    inline SAMPLE* at(long int x, long int y) const { return &rowdata[y][x * 3]; }
-    inline SAMPLE& at(long int x, long int y, unsigned char c) const { return rowdata[y][c + (x * 3)]; }
+    inline SAMPLE* row(long int y) const { return _rows->rowdata[y]; }
+    inline SAMPLE* at(long int x, long int y) const { return &(_rows->rowdata[y][x * 3]); }
+    inline SAMPLE& at(long int x, long int y, unsigned char c) const { return _rows->rowdata[y][c + (x * 3)]; }
 
-  private:
-
+    typedef std::shared_ptr<Image> shared_ptr;
   };
 
 }
