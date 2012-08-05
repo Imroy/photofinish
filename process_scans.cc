dab7885da3d52fc4f3d81688ea6d855288b541c9
Ian Tester <imroykun@gmail.com>
2012-08-05 21:26:34 +1000

    Add preview generation of files in the "works" directory. Add a configuration option for naming the works directory. Add some comments for blocks of code. Don't convert or generate previews if the file already exists and is newer than the original file. Use filename() instead of stem() when passing a filename to ImageFile::create because it uses replace_extension() anyway. Correctly handle files with multiple dots in the name.

--- process_scans.cc
+++ process_scans.cc
@@ -43,108 +43,167 @@ namespace po = boost::program_options;
 
 using namespace PhotoFinish;
 
+void make_preview(Image::ptr orig_image, Destination::ptr orig_dest, Tags::ptr filetags, ImageFile::ptr preview_file) {
+  Destination::ptr resized_dest = orig_dest->dupe();
+
+  resized_dest->set_depth(8);
+  resized_dest->set_jpeg(D_JPEG(60, 1, 1, true));
+
+  Frame::ptr frame(new Frame(orig_image->width() * 0.25, orig_image->height() * 0.25,
+			     0, 0,
+			     orig_image->width(), orig_image->height()));
+
+  Image::ptr resized_image = frame->crop_resize(orig_image, D_resize::lanczos(3));
+  preview_file->write(resized_image, resized_dest);
+  filetags->embed(preview_file);
+}
+
 int main(int argc, char* argv[]) {
+  // Variables that are to be loaded from the config file and command line
   bool do_conversion, do_preview;
-  po::options_description generic_options("Generic options");
-  generic_options.add_options()
-    ("help,h", "produce help message")
-    ("convert,C", po::bool_switch(&do_conversion), "Convert files")
-    ("preview,P", po::bool_switch(&do_preview), "Scale the image to a preview")
-    ;
+  fs::path convert_dir, works_dir;
+  std::string convert_format, preview_format;
+  typedef std::vector<fs::path> pathlist;
+  pathlist include_paths;
 
-  std::string convert_dir, convert_format, preview_format;
-  typedef std::vector<std::string> stringlist;
-  stringlist include_path;
-  po::options_description config_options("Configuration");
-  config_options.add_options()
-    ("convert-dir", po::value<std::string>(&convert_dir)->default_value("originals")->composing(), "Directory to place converted files")
-    ("convert-format", po::value<std::string>(&convert_format)->default_value("png")->composing(), "Format of converted images")
-    ("preview-format", po::value<std::string>(&preview_format)->default_value("jpeg")->composing(), "Format of preview images")
-    ("include-path,I", po::value< stringlist >(&include_path)->composing(), "include path for tag files")
-    ;
-
-  po::options_description cmdline_options;
-  cmdline_options.add(generic_options).add(config_options);
-
-  po::options_description visible_options("Allowed options");
-  visible_options.add(generic_options).add(config_options);
-
-  fs::path homedir = getenv("HOME");
-  fs::path configdir = homedir / ".config/PhotoFinish";
-  if (!fs::exists(configdir)) {
-    std::cerr << "Creating " << configdir << std::endl;
-    fs::create_directory(configdir);
-  }
-
+  // Define and load config file and command line options
   po::variables_map opts;
-  po::store(po::parse_command_line(argc, argv, cmdline_options), opts);
-  if (fs::exists(configdir / "process_scans.ini")) {
-    fs::ifstream config_file(configdir / "process_scans.ini", std::ios_base::in);
-    po::store(po::parse_config_file(config_file, config_options), opts);
-  }
-  po::notify(opts);
+  {
+    po::options_description generic_options("Generic options");
+    generic_options.add_options()
+      ("help,h", "produce help message")
+      ("convert,C", po::bool_switch(&do_conversion), "Convert files")
+      ("preview,P", po::bool_switch(&do_preview), "Scale the image to a preview")
+      ;
 
-  if (opts.count("help")) {
-    std::cerr << visible_options << std::endl;
-    return 1;
+    po::options_description config_options("Configuration");
+    config_options.add_options()
+      ("convert-dir", po::value<fs::path>(&convert_dir)->default_value("originals")->composing(), "Directory to place converted files")
+      ("convert-format", po::value<std::string>(&convert_format)->default_value("png")->composing(), "Format of converted images")
+      ("preview-format", po::value<std::string>(&preview_format)->default_value("jpeg")->composing(), "Format of preview images")
+      ("works-dir", po::value<fs::path>(&works_dir)->default_value("works")->composing(), "Directory to find works in progress")
+      ("include-path,I", po::value< pathlist >(&include_paths)->composing(), "include path for tag files")
+      ;
+
+    po::options_description cmdline_options;
+    cmdline_options.add(generic_options).add(config_options);
+
+    po::options_description visible_options("Allowed options");
+    visible_options.add(generic_options).add(config_options);
+
+    fs::path homedir = getenv("HOME");
+    fs::path configdir = homedir / ".config/PhotoFinish";
+    if (!fs::exists(configdir)) {
+      std::cerr << "Creating " << configdir << std::endl;
+      fs::create_directory(configdir);
+    }
+
+    po::store(po::parse_command_line(argc, argv, cmdline_options), opts);
+    if (fs::exists(configdir / "process_scans.ini")) {
+      fs::ifstream config_file(configdir / "process_scans.ini", std::ios_base::in);
+      po::store(po::parse_config_file(config_file, config_options), opts);
+    }
+    po::notify(opts);
+
+    // Display descriptions of command-line options if '--help' is given
+    if (opts.count("help")) {
+      std::cerr << visible_options << std::endl;
+      exit(1);
+    }
   }
 
   Tags::ptr defaulttags(new Tags);
-  for (stringlist::iterator i = include_path.begin(); i != include_path.end(); i++)
+  // Add paths for tag file searching
+  for (pathlist::iterator i = include_paths.begin(); i != include_paths.end(); i++)
     defaulttags->add_searchpath(*i);
 
+  // Load "default" in the search path list
   defaulttags->try_load("default");
+  // Load ".tags" from the current directory
   if (fs::exists(".tags"))
     defaulttags->load(".tags");
 
-  std::vector<fs::directory_entry> dir_list;
-  for (fs::directory_iterator di(fs::current_path()); di != fs::directory_iterator(); di++)
-    dir_list.push_back(*di);
-  sort(dir_list.begin(), dir_list.end());
-  for (std::vector<fs::directory_entry>::iterator di = dir_list.begin(); di != dir_list.end(); di++) {
-    if (!boost::iequals(di->path().extension().generic_string(), ".tif")
-	&& !boost::iequals(di->path().extension().generic_string(), ".tiff"))
-      continue;
+  if (do_conversion || do_preview) {
+    // std::sort won't work on fs::directory_iterator, so we have to put the entries in a vector and sort that
+    std::vector<fs::directory_entry> dir_list;
+    for (fs::directory_iterator di(fs::current_path()); di != fs::directory_iterator(); di++)
+      dir_list.push_back(*di);
+    sort(dir_list.begin(), dir_list.end());
 
-    try {
-      ImageFile::ptr infile = ImageFile::create(di->path());
+    // Iterate over all of the TIFF files in the current directory
+    for (std::vector<fs::directory_entry>::iterator di = dir_list.begin(); di != dir_list.end(); di++) {
+      if (!boost::iequals(di->path().extension().generic_string(), ".tif")
+	  && !boost::iequals(di->path().extension().generic_string(), ".tiff"))
+	continue;
 
-      Destination::ptr orig_dest(new Destination);
-      Image::ptr orig_image = infile->read(orig_dest);
-      Tags::ptr filetags = defaulttags->dupe();
-      filetags->extract(infile);
+      try {
+	ImageFile::ptr infile = ImageFile::create(di->path());
 
-      if (do_conversion) {
-	if (!fs::exists(convert_dir)) {
-	  std::cerr << "Creating directory \"" << convert_dir << "\"." << std::endl;
-	  fs::create_directory(convert_dir);
+	Destination::ptr orig_dest(new Destination);
+	Image::ptr orig_image = infile->read(orig_dest);
+	Tags::ptr filetags = defaulttags->dupe();
+	filetags->extract(infile);
+
+	try {
+	  ImageFile::ptr converted_file = ImageFile::create(convert_dir / di->path().filename(), convert_format);
+	  if (do_conversion && (!exists(converted_file->filepath()) || (last_write_time(converted_file->filepath()) < last_write_time(infile->filepath())))) {
+	    if (!fs::exists(convert_dir)) {
+	      std::cerr << "Creating directory " << convert_dir << "." << std::endl;
+	      fs::create_directory(convert_dir);
+	    }
+
+	    converted_file->write(orig_image, orig_dest);
+	    filetags->embed(converted_file);
+	  }
+	} catch (std::exception& ex) {
+	  std::cerr << ex.what() << std::endl;
 	}
 
-	ImageFile::ptr converted_file = ImageFile::create(convert_dir / di->path().stem(), convert_format);
-	converted_file->write(orig_image, orig_dest);
-	filetags->embed(converted_file);
+	try {
+	  ImageFile::ptr preview_file = ImageFile::create(di->path().filename(), preview_format);
+	  if (do_preview && (!exists(preview_file->filepath()) || (last_write_time(preview_file->filepath()) < last_write_time(infile->filepath()))))
+	    make_preview(orig_image, orig_dest, filetags, preview_file);
+	} catch (std::exception& ex) {
+	  std::cerr << ex.what() << std::endl;
+	}
+
+	std::cerr << "Moving " << di->path() << " to " << convert_dir / di->path().filename().native() << "\"" << std::endl;
+	rename(di->path().c_str(), (convert_dir / di->path().filename()).c_str());
+      } catch (std::exception& ex) {
+	std::cerr << ex.what() << std::endl;
       }
+    }
+  }
 
-      if (do_preview) {
-	Destination::ptr resized_dest = orig_dest->dupe();
+  if (do_preview) {
+    if (!exists(works_dir)) {
+      std::cerr << "Creating directory " << works_dir << "." << std::endl;
+      create_directory(works_dir);
+    }
+    std::vector<fs::directory_entry> dir_list;
+    for (fs::directory_iterator di(works_dir); di != fs::directory_iterator(); di++)
+      dir_list.push_back(*di);
+    sort(dir_list.begin(), dir_list.end());
 
-	resized_dest->set_depth(8);
-	resized_dest->set_jpeg(D_JPEG(60, 1, 1, true));
+    for (std::vector<fs::directory_entry>::iterator di = dir_list.begin(); di != dir_list.end(); di++) {
+      std::cerr << *di << std::endl;
+      try {
+	ImageFile::ptr infile = ImageFile::create(di->path());
+	ImageFile::ptr preview_file = ImageFile::create(di->path().filename(), preview_format);
 
-	Frame::ptr frame(new Frame(orig_image->width() * 0.25, orig_image->height() * 0.25,
-				   0, 0,
-				   orig_image->width(), orig_image->height()));
+	if (exists(preview_file->filepath())
+	    && (last_write_time(preview_file->filepath()) > last_write_time(infile->filepath())))
+	  continue;
 
-	Image::ptr resized_image = frame->crop_resize(orig_image, D_resize::lanczos(3));
-	ImageFile::ptr preview_file = ImageFile::create(di->path().stem(), preview_format);
-	preview_file->write(resized_image, resized_dest);
-	filetags->embed(preview_file);
+	Destination::ptr orig_dest(new Destination);
+	Image::ptr orig_image = infile->read(orig_dest);
+	Tags::ptr filetags = defaulttags->dupe();
+	filetags->extract(infile);
+
+	make_preview(orig_image, orig_dest, filetags, preview_file);
+      } catch (std::exception& ex) {
+	std::cerr << ex.what() << std::endl;
       }
-
-      std::cerr << "Moving " << di->path() << " to \"" << convert_dir << "/" << di->path().filename().native() << "\"" << std::endl;
-      rename(di->path().c_str(), (convert_dir / di->path().filename()).c_str());
-    } catch (std::exception& ex) {
-      std::cerr << ex.what() << std::endl;
     }
   }
 }
