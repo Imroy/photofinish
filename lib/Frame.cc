984fb512b6b7195f5873c3601ef4d5bca87a63ba
Ian Tester <imroykun@gmail.com>
2013-07-18 05:03:36 +1000

    Move some common code for rescaling images.
    We have to create the same two 1D kernels no matter which is applied first.

--- lib/Frame.cc
+++ lib/Frame.cc
@@ -38,16 +38,15 @@ namespace PhotoFinish {
   {}
 
   Image::ptr Frame::crop_resize(Image::ptr img, const D_resize& dr, bool can_free) {
+    auto scale_width = Kernel1Dvar::create(dr, _crop_x, _crop_w, img->width(), _width);
+    auto scale_height = Kernel1Dvar::create(dr, _crop_y, _crop_h, img->height(), _height);
+
     if (_width * img->height() < img->width() * _height) {
-      auto scale_width = Kernel1Dvar::create(dr, _crop_x, _crop_w, img->width(), _width);
       auto temp = scale_width->convolve_h(img, can_free);
-      auto scale_height = Kernel1Dvar::create(dr, _crop_y, _crop_h, img->height(), _height);
       return scale_height->convolve_v(temp, true);
     }
 
-    auto scale_height = Kernel1Dvar::create(dr, _crop_y, _crop_h, img->height(), _height);
     auto temp = scale_height->convolve_v(img, can_free);
-    auto scale_width = Kernel1Dvar::create(dr, _crop_x, _crop_w, img->width(), _width);
     return scale_width->convolve_h(temp, true);
   }
 
