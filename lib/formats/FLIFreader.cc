310ab601484bdfbddbd82e5c69df8a7da4277eab
Ian Tester <imroykun@gmail.com>
2019-09-01 03:03:48 +1000

    Nicer progress output when loading FLIFs

--- lib/formats/FLIFreader.cc
+++ lib/formats/FLIFreader.cc
@@ -29,7 +29,23 @@ namespace PhotoFinish {
   {}
 
   uint32_t flif_read_callback(uint32_t quality, int64_t bytes_read, uint8_t decode_over, void *user_data, void *context) {
-    std::cerr << "\r\tLoaded " << bytes_read << " bytes, quality=" << std::setprecision(2) << (quality * 0.01) << "%  ";
+    double mult;
+    std::string unit = "bytes";
+    if (bytes_read > 1099511627776) {
+      mult = 1.0 / 1099511627776;
+      unit = "TiB";
+    } else if (bytes_read > 1073741824) {
+      mult = 1.0 / 1073741824;
+      unit = "GiB";
+    } else if (bytes_read > 1048576) {
+      mult = 1.0 / 1048576;
+      unit = "MiB";
+    } else if (bytes_read > 1024) {
+      mult = 1.0 / 1024;
+      unit = "kiB";
+    }
+
+    std::cerr << "\r\tLoaded " << std::setprecision(2) << std::fixed << (bytes_read * mult) << " " << unit << ", progress=" << (quality * 0.01) << "%  ";
     return quality + 1000;
   }
 
